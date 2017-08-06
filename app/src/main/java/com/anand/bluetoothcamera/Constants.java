48534e313991a6c25b9f34d3377483e379e95468
Anand-M <anandm475@gmail.com>
2017-08-06 18:05:03 +0530

    first commit

---
+++ app/src/main/java/com/anand/bluetoothcamera/Constants.java
@@ -0,0 +1,15 @@
+package com.anand.bluetoothcamera;
+
+public interface Constants {
+    int MESSAGE_STATE_CHANGE = 1;
+    int MESSAGE_READ = 2;
+    int MESSAGE_WRITE = 3;
+    int MESSAGE_DEVICE_NAME = 4;
+    int MESSAGE_TOAST = 5;
+    int START_CAMERA_SERVICE = 6;
+    int STOP_CAMERA = 7;
+    int TAKE_PICTURE = 8;
+    int CAMERA_PREVIEW = 9;
+    String DEVICE_NAME = "device_name";
+    String TOAST = "toast";
+}
---
+++ app/src/main/java/com/anand/bluetoothcamera/IActionListner.java
@@ -0,0 +1,5 @@
+package com.anand.bluetoothcamera;
+
+public interface IActionListner {
+    public void onStartAction();
+}
